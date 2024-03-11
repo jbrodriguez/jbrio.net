ec0afae10a295688191b8168927dfd2451f75e83
Juan B. Rodriguez <jbrodriguez@gmail.com>
2024-03-11 08:26:34 -0500

    feat: fix favicon and misc copy changes

--- src/pages/CNAME.ts
+++
@@ -1,8 +0,0 @@
-import type { APIContext } from 'astro'
-import { SITE_DOMAIN } from '../consts'
-
-export function GET(_params: APIContext) {
-  return new Response(SITE_DOMAIN, {
-    headers: { 'Content-Type': 'text/plain' },
-  })
-}

4a6fb9d92e435d2f99333b222da9706cc3911167
Juan B. Rodriguez <jbrodriguez@users.noreply.github.com>
2024-03-11 12:33:12 -0500

    fix: post migration (#5)
    * fix: remove obsolete github action
    
    * feat: add new post script

---
+++ new-post.ts
@@ -0,0 +1,22 @@
+import { writeFileSync, existsSync, mkdirSync } from 'fs';
+import { join } from 'path';
+
+const folderName = process.argv[2];
+const today = new Date();
+const title = folderName;
+const cover = `${folderName}-feature.jpg`;
+const caption = ' © Juan B. Rodriguez';
+const status = 'draft';
+
+const folderPath = join('./src/content/posts', folderName);
+
+// Create the folder if it doesn't exist
+if (!existsSync(folderPath)) {
+  mkdirSync(folderPath);
+}
+
+// Write the markdown file
+writeFileSync(
+  `${folderPath}/index.md`,
+  `---\ntitle: '${title}'\ndate: ${today.toISOString()}\ncover: ./${cover}\ncaption: '${caption}'\nstatus: ${status}\ndescription: ''\n---`
+);
